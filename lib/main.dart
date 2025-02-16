3de2fe5d76f1fc9a9b47a84c502ca6d8eb390134
Kwode Great <obruchekwode@gmail.com>
2025-02-16 21:48:44 +0100

    added firebase to the for auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_todo_app/pages/sign_in_page.dart';
 import 'package:my_todo_app/pages/sign_up_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
